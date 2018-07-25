93e256d33a6cd9353f773b6908e29f7c4b23f906
aoaoo <41565601+aoaoo@users.noreply.github.com>
2018-07-25 04:12:42 +0200

    Add files via upload
    child error

--- pars.py
+++ pars.py
@@ -1,133 +1,218 @@
-'''infil = open('ndtv.xml')
-st = (infil.read())
-print(st)
-print(type(st))'''
+###Comments###
 
-'''#function
-def pipesel (x):
-  return 5 * x
+print('***Ez a kód a PUHY-HP, P és EP szériák esetében számolja össze a beltéri egységekhez szükséges jointokat (azaz a kültéri egységek összekötéséhez szükséges darabokat ehhez hozzá kell venni, ha van ilyen.***')
+print('***Jelenleg beta verzióban működik, az eredmények ellenőrzése szükséges.***')
+print('***A jelenlegi verzió nem számolja a szükséges szűkítők és az azokhoz szükséges extra csőmennyiségek mértékét. Ezzel összhangban nem a Mitsubishi szabványos elágazásait adja meg.***')
+print('***A végleges verzió esetén az eredmények ellenőrzése kimerülhet a Design Tool-ból exportálható és az innen számolt csőmennyiségek és gázrátöltés összehasonlításával.***')
+print('***Amennyiben a G11-nél vagy a Mitsubishi New Design Tool v1.90 frissebb megjelenik, úgy ellenőrizni kell a kódba égetett adatokat.***')
+print('***Használat ... in progress***')
+print('***Forráskód: https://github.com/aoaoo/NDT***')
+print()
+print("***Let's see the results***")
+print()
 
-print(pipesel(3))
-'''
+###Functions###
 
 #Check extra infos (modifications because of high distances)
 #The pipe diameter between the outdoor unit and the first branch
 def ou_branch (si, ty): #size and type of the outdoor unit
 	if ty == 'EP':
 		EP_dict = {
-		200: (10, 22),
-		250: (10, 22),
-		300: (10, 28),
-		350: (12, 28),
-		400: (12, 28),
-		450: (16, 28),
-		500: (16, 28),
-		550: (16, 28),
-		600: (16, 28),
-		650: (16, 28),
-		700: (20, 35),
-		750: (20, 35),
-		800: (20, 35),
-		850: (20, 42),
-		900: (20, 42),
-		950: (20, 42),
-		1000: (20, 42),
-		1050: (20, 42),
-		1100: (20, 42),
-		1150: (20, 42),
-		1200: (20, 42),
-		1250: (20, 42),
-		1300: (20, 42),
-		1350: (20, 42)		
+		200: [10, 22],
+		250: [10, 22],
+		300: [10, 28],
+		350: [12, 28],
+		400: [12, 28],
+		450: [16, 28],
+		500: [16, 28],
+		550: [16, 28],
+		600: [16, 28],
+		650: [16, 28],
+		700: [20, 35],
+		750: [20, 35],
+		800: [20, 35],
+		850: [20, 42],
+		900: [20, 42],
+		950: [20, 42],
+		1000: [20, 42],
+		1050: [20, 42],
+		1100: [20, 42],
+		1150: [20, 42],
+		1200: [20, 42],
+		1250: [20, 42],
+		1300: [20, 42],
+		1350: [20, 42]		
 		}
 		return EP_dict[si]
 	elif ty == 'HP':
 		HP_dict = {
-		200: (12, 20),
-		250: (12, 22),
-		400: (16, 28),
-		450: (16, 28),
-		500: (16, 28)		
+		200: [12, 20],
+		250: [12, 22],
+		400: [16, 28],
+		450: [16, 28],
+		500: [16, 28]		
 		}
 		return HP_dict[si]	
 	elif ty == 'P':
 		P_dict = {
-		200: (10, 22),
-		250: (10, 22),
-		300: (10, 22),
-		350: (12, 28),
-		400: (12, 28),
-		450: (16, 28),
-		500: (16, 28),
-		550: (16, 28),
-		600: (16, 28),
-		650: (16, 28),
-		700: (20, 35),
-		750: (20, 35),
-		800: (20, 35),
-		850: (20, 42),
-		900: (20, 42),
-		950: (20, 42),
-		1000: (20, 42),
-		1050: (20, 42),
-		1100: (20, 42),
-		1150: (20, 42),
-		1200: (20, 42),
-		1250: (20, 42),
-		1300: (20, 42),
-		1350: (20, 42)		
+		200: [10, 22],
+		250: [10, 22],
+		300: [10, 22],
+		350: [12, 28],
+		400: [12, 28],
+		450: [16, 28],
+		500: [16, 28],
+		550: [16, 28],
+		600: [16, 28],
+		650: [16, 28],
+		700: [20, 35],
+		750: [20, 35],
+		800: [20, 35],
+		850: [20, 42],
+		900: [20, 42],
+		950: [20, 42],
+		1000: [20, 42],
+		1050: [20, 42],
+		1100: [20, 42],
+		1150: [20, 42],
+		1200: [20, 42],
+		1250: [20, 42],
+		1300: [20, 42],
+		1350: [20, 42]		
 		}
 		return P_dict[si]
 	else:
-		return (-1, -1)
+		return [-1, -1]
 
 #Check extra infos (modifications because of high distances)
-#The pipe diameter between the outdoor unit and the first branch
-def branch_branch (si, ty): #size and type of the outdoor unit
+#The pipe diameter between the indoor unit and the branch
+def iu_branch (si, ty): #size of the connected indoor unit, and the type of the system (based on the data, now this second variable negigible)
 	if ty == 'EP':
 		EP_dict = {
-		200: (10, 22),
-		250: (10, 22),
-		300: (10, 28),
-		350: (12, 28),
-		400: (12, 28),
-		450: (16, 28),
-		500: (16, 28),
-		1350: (20, 42)		
+		15: [6, 12],
+		18: [6, 12],
+		20: [6, 12],
+		32: [6, 12],
+		35: [6, 12],
+		40: [6, 12],
+		42: [6, 12],
+		50: [6, 12],
+		63: [10, 16],
+		71: [10, 16],
+		80: [10, 16],
+		100: [10, 16],
+		125: [10, 16],
+		140: [10, 16],
+		200: [10, 20],
+		250: [10, 22],
 		}
 		return EP_dict[si]
 	elif ty == 'HP':
 		HP_dict = {
-		200: (12, 20),
-		250: (12, 22),
-		400: (16, 28),
-		450: (16, 28),
-		500: (16, 28)		
+		15: [6, 12],
+		18: [6, 12],
+		20: [6, 12],
+		32: [6, 12],
+		35: [6, 12],
+		40: [6, 12],
+		42: [6, 12],
+		50: [6, 12],
+		63: [10, 16],
+		71: [10, 16],
+		80: [10, 16],
+		100: [10, 16],
+		125: [10, 16],
+		140: [10, 16],
+		200: [10, 20],
+		250: [10, 22],
 		}
-		return HP_dict[si]	
+		return HP_dict[si]
 	elif ty == 'P':
 		P_dict = {
-		200: (10, 22),
-		250: (10, 22),
-		300: (10, 22),
-		1350: (20, 42)		
+		15: [6, 12],
+		18: [6, 12],
+		20: [6, 12],
+		32: [6, 12],
+		35: [6, 12],
+		40: [6, 12],
+		42: [6, 12],
+		50: [6, 12],
+		63: [10, 16],
+		71: [10, 16],
+		80: [10, 16],
+		100: [10, 16],
+		125: [10, 16],
+		140: [10, 16],
+		200: [10, 20],
+		250: [10, 22],
 		}
 		return P_dict[si]
 	else:
-		return (-1, -1)
+		return [-1, -1]
 		
-		
-		
+#Check extra infos (modifications because of high distances)
+#The pipe diameter between two branches
+def branch_branch (si, ty): #size of the total down-stream indoor capacity
+	if ty == 'EP':
+		if si <= 140:
+			return [10, 16]
+		elif si <= 200:
+			return [10, 20]
+		elif si <= 300:
+			return [10, 22]
+		elif si <= 400:
+			return [12, 28]
+		elif si <= 650:
+			return [16, 28]
+		elif si <= 800:
+			return [20, 35]
+		else:
+			return [20, 42]
+	elif ty == 'HP':
+		if si <= 140:
+			return [10, 16]
+		elif si <= 200:
+			return [10, 20]
+		elif si <= 300:
+			return [10, 22]
+		elif si <= 400:
+			return [12, 28]
+		elif si <= 650:
+			return [16, 28]
+		else:
+			return [-1,-1]
+	elif ty == 'P':
+		if si <= 140:
+			return [10, 16]
+		elif si <= 200:
+			return [10, 20]
+		elif si <= 300:
+			return [10, 22]
+		elif si <= 400:
+			return [12, 28]
+		elif si <= 650:
+			return [16, 28]
+		elif si <= 800:
+			return [20, 35]
+		else:
+			return [20, 42]
+	else:
+		return [-1, -1]
+
+
+###Import data###		
 import xml.etree.ElementTree as ET
 tree = ET.parse('ndtv.xml')
 root = tree.getroot()
 
-#var
+###var
 elemtyp = list() #Outdoor / Indoorunit, Branch
 size = list() #The capacity of the units
-joker1 = list() #Mitsu joint type; P, EP, HP
+joker1 = list() #Mitsubitsi system type; P, EP, HP
 parentid = list()
 a = 0 #Where a = ID (a = NDT_ID - 1)
+
+###Valus from string###
 
 while True:
 	try:
@@ -167,7 +252,7 @@ while True:
 				size[a] = (int(s[pos+i:pos+i+4]))
 			except ValueError:
 				continue
-		print('The size of the found outdoor unit is: ' + str(size[a]))
+		#print('The size of the found outdoor unit is: ' + str(size[a]))
 	elif s.find('Indoor') > 0:
 		elemtyp.append('Indoor')
 		joker1.append('')
@@ -198,14 +283,14 @@ while True:
 					size.append(int(s[pos+i:pos+i+3]))
 			except ValueError:
 				pass
-		print('The size of the indoor unit is: ' + str(size[a]))
+		#print('The size of the indoor unit is: ' + str(size[a]))
 	elif s.find('Branch') > 0:
 		elemtyp.append('Branch')
 		size.append(-1)
 		#Catch joker1
 		pos = s.find('CMY-Y')
 		joker1.append(s[pos:pos+13])
-		print('The type of the joint is: ' + joker1[a])
+		#print('The type of the joint is: ' + joker1[a])
 		#pareintID
 		pos = s.find('ParentUnitID')
 		try:
@@ -217,12 +302,11 @@ while True:
 		print(a)
 	a += 1
 
-#Building the tree
-#var
-leftsize = [0] * (a + 1)
-rightsize = [0] * (a + 1)
-leaf = [0] * (a + 1)
-leftchild = [-1] * (a + 1)  #just for the branch-IU connection
+###Building the tree###
+###var
+leftsize = [0] * (a + 1) #give the capacity connected on the 1st side 
+rightsize = [0] * (a + 1) #give the capacity connected on the 2nd side
+leftchild = [-1] * (a + 1)  #give the just for the branch-IU connection
 rightchild = [-1] * (a + 1) #just for the branch-IU connection
 
 i = a
@@ -230,7 +314,6 @@ for i in range(a, -1, -1):
 	if elemtyp[i] == 'Branch':
 		size[i] = leftsize[i] + rightsize[i]
 	elif elemtyp[i] == 'Indoor':
-		leaf[parentid[i]] += 1
 		if leftchild[parentid[i]] == -1:
 			leftchild[parentid[i]] = i
 		else:
@@ -240,19 +323,62 @@ for i in range(a, -1, -1):
 	else:
 		rightsize[parentid[i]] = size[i]
 
-#Branch size calculating
-#var
-bsize = list()
+###Branch size calculating###
+#This code calculate the amount of additional refirigent gas, and the length of the required copper pipe
+#For theese feature, need to get the pipe length, and the set of the model (when two or more block are need)
+#After it this code can calculate the 0th branch sizes (which connect the outdoor units if it necessary)
+
+###var
+pipdia = [[]] * (a + 1) #give diameter before of the equipment
+num_join = 1 #Number of joints (int, but bool may enoug later?)
 
 
+for i in range (0, a + 1):
+	if elemtyp[i] == 'Outdoor':
+		pipdia[i] = [0, 0]
+		pass #later
+	elif elemtyp[i] == 'Indoor':
+		pipdia[i] = iu_branch(size[i], joker1[0])
+	else:
+		if num_join == 1: #Not consecvent, other places the ID = 0 means OU, ID = 1 => branch
+			pipdia[i] = ou_branch(size[0], joker1[0])
+		else:
+			pipdia[i] = branch_branch(size[i], joker1[0])
+		num_join += 1
+
+
+###var
+b = 0 #Temp variable, joint ID (0::num_join-1)
+joint_g = [[]] * (num_join) #joints for gas
+joint_f = [[]] * (num_join) #joints for fluid
+
+if size[0] >= 400:
+	print('0th joint may need')#->output file
+for i in range (0, a + 1):
+	if elemtyp[i] == 'Branch':
+		print(i)
+		joint_f[b] = [pipdia[i][1], pipdia[leftchild[i]][1], pipdia[rightchild[i]][1]] 
+		joint_g[b] = [pipdia[i][2], pipdia[leftchild[i]][2], pipdia[rightchild[i]][1]] 
+		b += 1
+		print(i)
+	
+	
+	
+
+print()
+print('***End***')
+print()
 '''print('****Test section start here******')
 int('200')
 print(len(size))
 print('****End******')
 '''
 
-print(leaf)
+print(str(leftchild[2]))
+print(joint_f)
+print(joint_g)
+print(num_join)
+print(size)
+print(pipdia)
 print(leftchild)
 print(rightchild)
-print('****End of code****')
-input()
