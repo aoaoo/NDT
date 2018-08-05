52e7d3795315a19713eb3e71103f44e2c7977e4f
aoaoo <41565601+aoaoo@users.noreply.github.com>
2018-08-06 01:33:59 +0200

    Add files via upload
    point corrected

--- pars.py
+++ pars.py
@@ -54,11 +54,10 @@ def ou_branch (si, ty): #size and type of the outdoor unit
 		200: [12, 20],
 		250: [12, 22],
 		400: [16, 28],
-		450: [16, 28],
 		500: [16, 28]		
 		}
 		return HP_dict[si]	
-	elif ty == 'P':
+	else: #ty == 'P'
 		P_dict = {
 		200: [10, 22],
 		250: [10, 22],
@@ -86,8 +85,7 @@ def ou_branch (si, ty): #size and type of the outdoor unit
 		1350: [20, 42]		
 		}
 		return P_dict[si]
-	else:
-		return [-1, -1]
+
 
 #Check extra infos (modifications because of high distances)
 #The pipe diameter between the indoor unit and the branch
@@ -111,7 +109,7 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		125: [10, 16],
 		140: [10, 16],
 		200: [10, 20],
-		250: [10, 22],
+		250: [10, 22]
 		}
 		return EP_dict[si]
 	elif ty == 'HP':
@@ -133,10 +131,10 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		125: [10, 16],
 		140: [10, 16],
 		200: [10, 20],
-		250: [10, 22],
+		250: [10, 22]
 		}
 		return HP_dict[si]
-	elif ty == 'P':
+	else: #ty == 'P'
 		P_dict = {
 		15: [6, 12],
 		18: [6, 12],
@@ -155,11 +153,10 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		125: [10, 16],
 		140: [10, 16],
 		200: [10, 20],
-		250: [10, 22],
+		250: [10, 22]
 		}
 		return P_dict[si]
-	else:
-		return [-1, -1]
+
 		
 #Check extra infos (modifications because of high distances)
 #The pipe diameter between two branches
@@ -192,7 +189,7 @@ def branch_branch (si, ty): #size of the total down-stream indoor capacity (and 
 			return [16, 28]
 		else:
 			return [-1,-1]
-	elif ty == 'P':
+	else: #ty == 'P'
 		if si <= 140:
 			return [10, 16]
 		elif si <= 200:
@@ -207,8 +204,6 @@ def branch_branch (si, ty): #size of the total down-stream indoor capacity (and 
 			return [20, 35]
 		else:
 			return [20, 42]
-	else:
-		return [-1, -1]
 
 ###Values from string###
 def get_num (s, typ): #String and type of the expected value; need: 'import re'
@@ -219,7 +214,74 @@ def get_num (s, typ): #String and type of the expected value; need: 'import re'
 	else: #positive int
 		return int(re.search(r'\d+', s).group(0))
 
-###Import data###		
+#bend length correction
+def bend_len_corr (si, ty): #size of the connected indoor unit, and the type of the system
+	if ty == 'EP':
+		EP_dict = {
+			200: 0.42,
+			250: 0.42,
+			300: 0.47,
+			350: 0.47,
+			400: 0.50,
+			450: 0.50,
+			500: 0.50,
+			550: 0.50,
+			600: 0.50,
+			650: 0.50,
+			700: 0.70,
+			750: 0.70,
+			800: 0.70,
+			850: 0.80,
+			900: 0.80,
+			950: 0.80,
+			1000: 0.80,
+			1050: 0.80,
+			1100: 0.80,
+			1150: 0.80,
+			1200: 0.80,
+			1250: 0.80,
+			1300: 0.80,
+			1350: 0.80
+		}
+		return EP_dict[si]
+	elif ty == 'HP':
+		HP_dict = {
+			200: 0.30,
+			250: 0.35,
+			400: 0.50,
+			500: 0.50
+		}
+		return HP_dict[si]
+	else: #ty == 'P'
+		P_dict = {
+			200: 0.42,
+			250: 0.42,
+			300: 0.47,
+			350: 0.47,
+			400: 0.50,
+			450: 0.50,
+			500: 0.50,
+			550: 0.50,
+			600: 0.50,
+			650: 0.50,
+			700: 0.70,
+			750: 0.70,
+			800: 0.70,
+			850: 0.80,
+			900: 0.80,
+			950: 0.80,
+			1000: 0.80,
+			1050: 0.80,
+			1100: 0.80,
+			1150: 0.80,
+			1200: 0.80,
+			1250: 0.80,
+			1300: 0.80,
+			1350: 0.80
+		}
+		return P_dict[si]
+
+###Import data###
 import xml.etree.ElementTree as ET
 tree = ET.parse('ndtv.xml')
 root = tree.getroot()
@@ -230,157 +292,65 @@ size = list() #The capacity of the units
 joker1 = list() #Random joker
 parentid = list()
 piplen = list() #Pipe length before the unit #Expect outdoor unit
+c_piplen = list() #The equivalent pipe length
 numbend	= list() #Number of bent before the unit #Expect the outdoort unit
 a = 0 #Where a = ID (a = NDT_ID - 1)
 systyp = '' #system type: P, HP, EP
-'''lengthA
-lengthB
-lengthC
-bendA
-bendB
-bendC''' #The outdoor unit typical values
-
+dbou_unit = 1
+#lenA lenB lenC nenD lenE bendA bendB bendC bendD bendE #Tho outdoor units piping length
 
 for unit in root.iter('Unit'):
 	elemtyp.append(unit.get('Model'))
 	if elemtyp[a] == 'Outdoor':
 		size.append(get_num(unit.get('ModelName'),'pint'))
-		parentid.append(0)
+		parentid.append(-1)
 		piplen.append(0)
 		numbend.append(0)
 		systyp = (re.search(r'[-][E,H]?[P]', unit.get('ModelName')).group(0)).replace('-', '')
+		lenA = get_num(unit.get('PipeLengthA'),'float')
+		bendA = get_num(unit.get('BendA'), 'pint')
+		piplen.append(lenA)
+		numbend.append(bendA)
+		try:
+			lenB = get_num(unit.get('PipeLengthB'),'float'); lenB
+			bendB = get_num(unit.get('BendB'), 'pint')
+			piplen.append(lenB)
+			numbend.append(bendB)
+			dbou_unit += 1
+			lenC = get_num(unit.get('PipeLengthC'),'float')
+			bendC = get_num(unit.get('BendC'), 'pint')
+			piplen.append(lenC)
+			numbend.append(bendC)
+			dbou_unit += 1
+			lenD = get_num(unit.get('PipeLengthD'),'float')
+			bendD = get_num(unit.get('BendD'), 'pint')
+			piplen.append(lenD)
+			numbend.append(bendD)
+			dbou_unit += 1
+			lenE = get_num(unit.get('PipeLengthE'),'float')
+			bendE = get_num(unit.get('BendE'), 'pint')
+			piplen.append(lenE)
+			numbend.append(bendE)
+		except AttributeError:
+			pass
 	elif elemtyp[a] == 'Indoor':
 		size.append(get_num(unit.get('ModelName'),'pint'))
-		parentid.append(int(unit.get('ParentUnitID')))
+		parentid.append(int(unit.get('ParentUnitID')) - 1)
 		piplen.append(get_num(unit.get('PipeLength'),'float'))
 		numbend.append(get_num(unit.get('Bend'),'pint'))
-	elif elemtyp[a] == 'Branch':
+	elif elemtyp[a] == 'Branch': #Jump the frist branch pilength, and bend values, use lenA;;lenE and bendA;;bendE
 		size.append(0)
-		parentid.append(int(unit.get('ParentUnitID')))
-		piplen.append(get_num(unit.get('PipeLength'),'float'))
-		numbend.append(get_num(unit.get('Bend'),'pint'))
+		parentid.append(int(unit.get('ParentUnitID')) - 1)
+		if a != 1:
+			piplen.append(get_num(unit.get('PipeLength'),'float'))
+			numbend.append(get_num(unit.get('Bend'),'pint'))
 	else:
 		print('Unknown device type in sequence (0;;unit): ' + str(a))#Err mess
+	bend_len_corr(200, 'EP')
+	c_piplen.append(float(numbend[a]*bend_len_corr(size[0], systyp) + piplen[a]))
 	a += 1
 a -= 1
-
-'''
-while True:
-	try:
-		root[0][2][a]
-	except IndexError:
-		a -= 1
-		break
-	s = str(root[0][2][a].attrib)
-	if s.find('Outdoor') > 0:
-		elemtyp.append('Outdoor')
-		#pareintID
-		parentid.append(-1)
-		#HP, P, EP -> joker1
-		#joker1.append('')
-		if s.find('PUHY') > 0:
-			pos = s.find('PUHY')
-		else:
-			print("This system not allowed") #Crete a better sys mess
-		if s[pos+5:pos+7] == 'EP':
-			joker1.append('EP')
-		elif s[pos+5:pos+7] == 'HP':
-			joker1.append('HP')
-		else:
-			joker1.append('P')
-		#outdoor unit size -> size
-		pos = s.find('PUHY')
-		for i in range(6, 8, 1):
-			if len(size) > a:
-				break
-			try:
-				int(s[pos+i:pos+i+3])
-				size.append(int(s[pos+i:pos+i+3]))
-			except ValueError:
-				continue
-			try:
-				int(s[pos+i:pos+i+4])
-				size[a] = (int(s[pos+i:pos+i+4]))
-			except ValueError:
-				continue
-		#pipelength
-		piplen.append(0.0)
-		#number of bent
-		numbend.append(0)
-		#print('The size of the found outdoor unit is: ' + str(size[a]))
-	elif s.find('Indoor') > 0:
-		elemtyp.append('Indoor')
-		joker1.append('')
-		#pareintID
-		pos = s.find('ParentUnitID')
-		try:
-			int(s[pos+16:pos+18])
-			parentid.append(int(s[pos+16:pos+18])-1)
-		except ValueError:
-			parentid.append(int((s[pos+16]))-1)
-		#indoor unit size -> size
-		pos = s.find('ModelName')
-		if s.find('PWFY') > 0:
-			print('Error, bad construction')#Create correct err mess
-		for i in range(18, 21):
-			if len(size) > a:
-				break
-			try:
-				int(s[pos+i:pos+i+2])
-				size.append(int(s[pos+i:pos+i+2]))
-			except ValueError:
-				pass
-			try:
-				int(s[pos+i:pos+i+3])
-				if len(size) > a:
-					size[a] = int(s[pos+i:pos+i+3])
-				else:
-					size.append(int(s[pos+i:pos+i+3]))
-			except ValueError:
-				pass
-		#pipelength
-		pos = s.find('PipeLength')
-		piplen.append(0.0)
-		for i in range (1, 6):
-			try:
-				float(s[pos+14:pos+14+i].replace(",","."))
-				if piplen[a] <= float(s[pos+14:pos+14+i].replace(",",".")):
-					piplen[a] = float(s[pos+14:pos+14+i].replace(",","."))
-			except ValueError:
-				continue
-		#number of bent
-		pos = s.find('Bend')
-		print(s[pos:pos+11])
-		#print('The size of the indoor unit is: ' + str(size[a]))
-	elif s.find('Branch') > 0:
-		elemtyp.append('Branch')
-		size.append(-1)
-		#Catch joker1
-		pos = s.find('CMY-Y')
-		joker1.append(s[pos:pos+13])
-		#print('The type of the joint is: ' + joker1[a])
-		#pareintID
-		pos = s.find('ParentUnitID')
-		try:
-			int(s[pos+16:pos+18])
-			parentid.append(int(s[pos+16:pos+18])-1)
-		except ValueError:
-			parentid.append(int((s[pos+16]))-1)
-		#pipelength
-		pos = s.find('PipeLength')
-		piplen.append(0.0)
-		for i in range (1, 6):
-			try:
-				float(s[pos+14:pos+14+i].replace(",","."))
-				if piplen[a] <= float(s[pos+14:pos+14+i].replace(",",".")):
-					piplen[a] = float(s[pos+14:pos+14+i].replace(",","."))
-			except ValueError:
-				continue
-	else:
-		print(a)
-	a += 1
-'''
-
+	
 ###Building the tree###
 ###var
 leftsize = [0] * (a + 1) #give the capacity connected on the 1st side 
@@ -389,12 +359,12 @@ leftchild = [-1] * (a + 1)  #give the just for the branch-IU connection
 rightchild = [-1] * (a + 1) #just for the branch-IU connection
 
 i = a
-for i in range(a, -1, -1):
+for i in range(a, 0, -1):
 	if elemtyp[i] == 'Branch':
 		size[i] = leftsize[i] + rightsize[i]
 	if leftchild[parentid[i]] == -1:
 		leftchild[parentid[i]] = i
-		leftsize[parentid[i]] = size[i]
+		leftsize[parentid[i]] = size[i]	
 	else:
 		rightchild[parentid[i]] = i
 		rightsize[parentid[i]] = size[i]
@@ -406,7 +376,7 @@ for i in range(a, -1, -1):
 
 ###var
 pipdia = [[]] * (a + 1) #give diameter before of the equipment
-pipdia_len = [0, 0, 0, 0, 0, 0, 0, 0, 0] #6, 10 12, 16, 20, 22, 28, 35, 42
+pipdia_len = [0, 0, 0, 0, 0, 0, 0, 0, 0] #6, 10 12, 16, 20, 22, 28, 35, 42 #To summarize the total pipe length group by diameter
 knowndia = { #known diameters to create pipdia_len vector
 		6: 0,
 		0: 0, #delete the zero value
@@ -433,6 +403,38 @@ for i in range (0, a + 1):
 		else:
 			pipdia[i] = branch_branch(size[i], systyp)
 		num_join += 1
+
+#In case of P250-300, EP250-300 if the max equvivalent distance between the outdoor unit and the indoor unit is equal or more then 90/40 m, the outdoor unit liquid pipe has to reselect
+if (size[0] == 300 or size[0] == 250) and (systyp == 'P' or systyp == 'EP'): 
+	sumlen = c_piplen
+	if size[0] == 300:
+		crit = 40
+	else:
+		crit = 90
+	#Mert bugos az NDT XML exportja, és az 1. joint távolsága néha fals (lenA-val redundánsan ugyan az kéne, hogy legyen)
+	if rightchild[1] != -1:
+		sumlen[rightchild[1]] += lenA
+	if leftchild[1] != -1:
+		sumlen[leftchild[1]] += lenA
+	for i in range(2, a + 1):
+		if rightchild[i] != -1:
+			sumlen[rightchild[i]] += c_piplen[i]
+		if leftchild[i] != -1:
+			sumlen[leftchild[i]] += c_piplen[i]
+		if sumlen[i] >= crit:
+			if size[0] == 300:
+				pipdia[1][0] = 12
+			else:
+				pipdia[1][0] = 12
+			break
+
+#correction of the pipe diameter (if the upper one is smaller)
+for i in range(1, a + 1):
+	for j in range(0, 2):
+		if pipdia[leftchild[i]][j] > pipdia[i][j] and elemtyp[leftchild[i]] != 'Indoor':
+			pipdia[leftchild[i]][j] = pipdia[i][j]
+		elif pipdia[rightchild[i]][j] > pipdia[i][j] and elemtyp[rightchild[i]] != 'Indoor':
+			pipdia[rightchild[i]][j] = pipdia[i][j]
 
 #pipelength
 for i in range(0, a + 1):
@@ -484,7 +486,6 @@ print('pipe length 6, 10 12, 16, 20, 22, 28, 35, 42: ' + str(pipdia_len))
 print('****Test section starts here******')
 
 '''
-
 from openpyxl import Workbook
 wb = Workbook()
 ws = wb.active
@@ -503,7 +504,9 @@ print('****End******')
 #bent -> pipe length in the export?
 #outdoor unit joint calculate
 #calculate the special cases
-#create exőport file
-#Summarize the export files
+#create export file
+#Summarize more export files
 #Check from the NDT .xlsx export file
 #a (new)index error correction
+#-1 return value -> error message; + do not cause error
+#check the 1st bend xml bug
