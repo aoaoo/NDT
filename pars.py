a99a49dad7510af7a65a403fa00d6900292faca2
aoaoo <41565601+aoaoo@users.noreply.github.com>
2018-09-21 15:39:56 +0200

    Add files via upload
    Final version

--- pars.py
+++ pars.py
@@ -1,16 +1,16 @@
 ###Comments###
 #Python v3.5.0
 #openpyxl v2.5.3
-print('***Ez a kód a PUHY-HP, P és EP szériák esetében számolja össze a beltéri egységekhez szükséges jointokat (azaz a kültéri egységek összekötéséhez szükséges darabokat ehhez hozzá kell venni, ha van ilyen.***')
-print('***Jelenleg beta verzióban működik, az eredmények ellenőrzése szükséges.***')
-print('***A jelenlegi verzió nem számolja a szükséges szűkítők és az azokhoz szükséges extra csőmennyiségek mértékét. Ezzel összhangban nem a Mitsubishi szabványos elágazásait adja meg.***')
-print('***A végleges verzió esetén az eredmények ellenőrzése kimerülhet a Design Tool-ból exportálható és az innen számolt csőmennyiségek összehasonlításával.***')
-print('***Az ellenőrzésnél figyelni kell, hogy a kültériek összecsövezésére szolgáló csőmennyiség nincs benne ennek a programnak a kimenetében.***')
-print('***Ezek mellett a kültériek összekötésére szolgáló csövekből a NDT jellemzően az egyik géptől érkező "gáz" csövet nem számolja.***')
-print('***Amennyiben a G11-nél vagy a Mitsubishi New Design Tool v1.90 frissebb megjelenik, úgy ellenőrizni kell a kódba égetett adatokat.***')
-print('***A program a kültéri egységek összekötéséhez szükséges csőmennyiséget és jointo(ka)t nem számítja, ezt kézzel hozzá kell adni!***')
-print('***Használat ... in progress***')
-print('***Forráskód: https://github.com/aoaoo/NDT***')
+print('***Ez a kód a PUHY-HP, -P és -EP szériák esetében számolja össze a beltéri egységekhez szükséges jointokat, szűkítőket és csöveket (azaz a kültéri egységek összekötéséhez szükséges darabokat ehhez hozzá kell venni, ha van ilyen). \nAmennyiben 56-osnál nagyobb szűkítő kellene valahol, akkor a kódot ki kell egészíteni, jelenleg nem tud annál nagyobb átmérővel dolgozni.')
+print('***Jelenleg beta verzióban működik, az eredmények ellenőrzése szükséges.')
+print('***A program nem a Mitsubishi szabványos elágazásait adja meg.')
+print('***A végleges verzió esetén az eredmények ellenőrzése kimerülhet a Design Tool-ból exportálható és az innen számolt csőmennyiségek összehasonlításával.')
+print('***Az ellenőrzésnél figyelni kell, hogy a kültériek összecsövezésére szolgáló csőmennyiség nincs benne ennek a programnak a kimenetében.')
+print('***Ezek mellett a kültériek összekötésére szolgáló csövekből a NDT jellemzően az egyik géptől érkező "gáz" csövet nem számolja.')
+print('***Amennyiben a G11-nél vagy a Mitsubishi New Design Tool v1.90 frissebb megjelenik, úgy ellenőrizni kell a kódba égetett adatokat.')
+print('***A program a kültéri egységek összekötéséhez szükséges csőmennyiséget és jointo(ka)t nem számítja, ezt kézzel hozzá kell adni!')
+print('***Használat \n1, Exportáljuk ki a NDT-ból az xml fájlt. \n2, Futtassuk ezt a programot úgy, hogy a program egy mappában van az exportált fájllal. \n3, Figyelni kell rá, hogy a programnak nincs jogosultsága létező excell fájlt felülírni, ezért [xml_filename]_joint.xlsx néven ne legyen a mappában fájl. \n4, A futás után ellenőrizzük az eredményeket: A számolt csőmennyiségeket, esetleg az egyéb kimeneti értékeit.')
+print('***Forráskód: https://github.com/aoaoo/NDT')
 print()
 print("***Begin***")
 print()
@@ -20,6 +20,7 @@ import string #string
 import re #regexp
 import sys #system exit
 import time #wait
+from operator import itemgetter #Short lists
 
 ###Functions###
 
@@ -33,33 +34,33 @@ def ou_branch (si, ty): #size and type of the outdoor unit
 		300: [10, 28],
 		350: [12, 28],
 		400: [12, 28],
-		450: [16, 28],
-		500: [16, 28],
-		550: [16, 28],
-		600: [16, 28],
-		650: [16, 28],
-		700: [20, 35],
-		750: [20, 35],
-		800: [20, 35],
-		850: [20, 42],
-		900: [20, 42],
-		950: [20, 42],
-		1000: [20, 42],
-		1050: [20, 42],
-		1100: [20, 42],
-		1150: [20, 42],
-		1200: [20, 42],
-		1250: [20, 42],
-		1300: [20, 42],
-		1350: [20, 42]		
+		450: [15, 28],
+		500: [15, 28],
+		550: [15, 28],
+		600: [15, 28],
+		650: [15, 28],
+		700: [18, 35],
+		750: [18, 35],
+		800: [18, 35],
+		850: [18, 42],
+		900: [18, 42],
+		950: [18, 42],
+		1000: [18, 42],
+		1050: [18, 42],
+		1100: [18, 42],
+		1150: [18, 42],
+		1200: [18, 42],
+		1250: [18, 42],
+		1300: [18, 42],
+		1350: [18, 42]		
 		}
 		return EP_dict[si]
 	elif ty == 'HP':
 		HP_dict = {
-		200: [12, 20],
+		200: [12, 18],
 		250: [12, 22],
-		400: [16, 28],
-		500: [16, 28]		
+		400: [15, 28],
+		500: [15, 28]		
 		}
 		return HP_dict[si]	
 	else: #ty == 'P'
@@ -69,25 +70,25 @@ def ou_branch (si, ty): #size and type of the outdoor unit
 		300: [10, 22],
 		350: [12, 28],
 		400: [12, 28],
-		450: [16, 28],
-		500: [16, 28],
-		550: [16, 28],
-		600: [16, 28],
-		650: [16, 28],
-		700: [20, 35],
-		750: [20, 35],
-		800: [20, 35],
-		850: [20, 42],
-		900: [20, 42],
-		950: [20, 42],
-		1000: [20, 42],
-		1050: [20, 42],
-		1100: [20, 42],
-		1150: [20, 42],
-		1200: [20, 42],
-		1250: [20, 42],
-		1300: [20, 42],
-		1350: [20, 42]		
+		450: [15, 28],
+		500: [15, 28],
+		550: [15, 28],
+		600: [15, 28],
+		650: [15, 28],
+		700: [18, 35],
+		750: [18, 35],
+		800: [18, 35],
+		850: [18, 42],
+		900: [18, 42],
+		950: [18, 42],
+		1000: [18, 42],
+		1050: [18, 42],
+		1100: [18, 42],
+		1150: [18, 42],
+		1200: [18, 42],
+		1250: [18, 42],
+		1300: [18, 42],
+		1350: [18, 42]		
 		}
 		return P_dict[si]
 
@@ -107,13 +108,13 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		40: [6, 12],
 		42: [6, 12],
 		50: [6, 12],
-		63: [10, 16],
-		71: [10, 16],
-		80: [10, 16],
-		100: [10, 16],
-		125: [10, 16],
-		140: [10, 16],
-		200: [10, 20],
+		63: [10, 15],
+		71: [10, 15],
+		80: [10, 15],
+		100: [10, 15],
+		125: [10, 15],
+		140: [10, 15],
+		200: [10, 18],
 		250: [10, 22]
 		}
 		return EP_dict[si]
@@ -129,13 +130,13 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		40: [6, 12],
 		42: [6, 12],
 		50: [6, 12],
-		63: [10, 16],
-		71: [10, 16],
-		80: [10, 16],
-		100: [10, 16],
-		125: [10, 16],
-		140: [10, 16],
-		200: [10, 20],
+		63: [10, 15],
+		71: [10, 15],
+		80: [10, 15],
+		100: [10, 15],
+		125: [10, 15],
+		140: [10, 15],
+		200: [10, 18],
 		250: [10, 22]
 		}
 		return HP_dict[si]
@@ -151,13 +152,13 @@ def iu_branch (si, ty): #size of the connected indoor unit, and the type of the 
 		40: [6, 12],
 		42: [6, 12],
 		50: [6, 12],
-		63: [10, 16],
-		71: [10, 16],
-		80: [10, 16],
-		100: [10, 16],
-		125: [10, 16],
-		140: [10, 16],
-		200: [10, 20],
+		63: [10, 15],
+		71: [10, 15],
+		80: [10, 15],
+		100: [10, 15],
+		125: [10, 15],
+		140: [10, 15],
+		200: [10, 18],
 		250: [10, 22]
 		}
 		return P_dict[si]
@@ -173,9 +174,9 @@ def Miu_branch (si): #In this case system type is not important
 	30: [6, 10],
 	40: [6, 10],
 	45: [6, 12], #unit 50, because of the size correction
-	60: [6, 16],
-	71: [10, 16],
-	80: [10, 16]
+	60: [6, 15],
+	71: [10, 15],
+	80: [10, 15]
 	}
 	return M_dict[si]
 
@@ -184,47 +185,47 @@ def Miu_branch (si): #In this case system type is not important
 def branch_branch (si, ty): #size of the total down-stream indoor capacity (and the outdoor unit type)
 	if ty == 'EP':
 		if si <= 140:
-			return [10, 16]
+			return [10, 15]
 		elif si <= 200:
-			return [10, 20]
+			return [10, 18]
 		elif si <= 300:
 			return [10, 22]
 		elif si <= 400:
 			return [12, 28]
 		elif si <= 650:
-			return [16, 28]
+			return [15, 28]
 		elif si <= 800:
-			return [20, 35]
+			return [18, 35]
 		else:
-			return [20, 42]
+			return [18, 42]
 	elif ty == 'HP':
 		if si <= 140:
-			return [10, 16]
+			return [10, 15]
 		elif si <= 200:
-			return [10, 20]
+			return [10, 18]
 		elif si <= 300:
 			return [10, 22]
 		elif si <= 400:
 			return [12, 28]
 		elif si <= 650:
-			return [16, 28]
+			return [15, 28]
 		else:
 			return [-1,-1]
 	else: #ty == 'P'
 		if si <= 140:
-			return [10, 16]
+			return [10, 15]
 		elif si <= 200:
-			return [10, 20]
+			return [10, 18]
 		elif si <= 300:
 			return [10, 22]
 		elif si <= 400:
 			return [12, 28]
 		elif si <= 650:
-			return [16, 28]
+			return [15, 28]
 		elif si <= 800:
-			return [20, 35]
+			return [18, 35]
 		else:
-			return [20, 42]
+			return [18, 42]
 
 ###Values from string###
 def get_num (s, typ): #String and type of the expected value; need: 'import re'
@@ -364,11 +365,14 @@ dbou = 1 #Number of the outdoor unit
 minh = 0 #Minimum height by the IU-s
 maxh = 0 #Maximum height by the IU-s
 #lenA lenB lenC nenD lenE bendA bendB bendC bendD bendE #Tho outdoor units piping length
+info = '' #Informations to the excel table
 
 for unit in root.iter('Unit'):
 	elemtyp.append(unit.get('Model'))
 	if elemtyp[a] == 'Outdoor':
 		size.append(get_num(unit.get('ModelName'),'pint'))
+		if size[0] > 700:
+			info = info + ' A kültéri közeli jointokat ellenőrizni kell, hogy szükség van-e 56-osnál nagyobb szűkítőre! '
 		parentid.append(-1)
 		piplen.append(0)
 		numbend.append(0)
@@ -379,9 +383,10 @@ for unit in root.iter('Unit'):
 		numbend.append(bendA)
 		iutyp.append('VRF')
 		try:
-			lenB = get_num(unit.get('PipeLengthB'),'float'); lenB
+			lenB = get_num(unit.get('PipeLengthB'),'float')
 			bendB = get_num(unit.get('BendB'), 'pint')
 			dbou += 1
+			info = info + ' Több kültéri egység esetén az őket összekötő jointokat, és csöveket a kód nem számolja, ezeket kézzel kell hozzáadni!'
 			lenC = get_num(unit.get('PipeLengthC'),'float')
 			bendC = get_num(unit.get('BendC'), 'pint')
 			piplen[1] = lenC
@@ -456,30 +461,34 @@ pipdia = [[]] * (a + 1) #give diameter before of the equipment
 incdia = [False] * (a + 1) #Increased the liquid pipe diameter, because height limitation or exceeding the 40m
 bunit = 0 #The height of the base unit
 #act_paid  actual parendtID (by function height limit test)
-pipdia_len = [0, 0, 0, 0, 0, 0, 0, 0, 0] #6, 10 12, 16, 20, 22, 28, 35, 42 #To summarize the total pipe length group by diameter
+pipdia_len = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #6, 10 12, 15, 18, 22, 28, 35, 42, 56 #To summarize the total pipe length group by diameter
+knowndia_reducer_len = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #8, 10 12, 15, 18, 22, 28, 35, 42, 56 #Pipe length to the reducer manufacturing #In this case instead of D6 use D8!!
+reducer = list()
 knowndia = { #known diameters to create pipdia_len vector
 		6: 0,
 		0: 0, #delete the zero value
 		10: 1,
 		12: 2,
-		16: 3,
-		20: 4,
+		15: 3,
+		18: 4,
 		22: 5,
 		28: 6,
 		35: 7,
-		42: 8
+		42: 8,
+		56: 9
 		}
 
 knowndia_inv = { #known diameters to create increase pipe diameter
 		0: 6,
 		1: 10,
 		2: 12,
-		3: 16,
-		4: 20,
+		3: 15,
+		4: 18,
 		5: 22,
 		6: 28,
 		7: 35,
-		8: 42
+		8: 42,
+		9: 56
 		}
 
 num_join = 1 #Number of joints (int, but bool may enoug later?)
@@ -563,7 +572,6 @@ for i in range(0, a + 1):
 b = 0 #Temp variable, joint ID (0::num_join-1)
 joint = [[]] * (num_join*2) #joints
 joint_s = list()
-joint_num = list()
 
 #Get the joints
 joint[b] = [0,0,0]
@@ -578,74 +586,185 @@ for i in range (0, a + 1):
 		b += 2
 
 #summarize joints
-joint_s.append(joint[0])
-joint_num.append(1)
+joint_s.append([joint[0][0], joint[0][1], joint[0][2], 1])
 for i in range (1, len(joint)):
 	exist = False
 	for j in range(0, len(joint_s)):
-		if joint_s[j] == joint[i]:
-			joint_num[j] += 1
+		if joint_s[j][0:3] == joint[i]:
+			joint_s[j][3] += 1
 			exist = True
 			break
 	if exist == True:
 		continue
 	else:
-		joint_s.append(joint[i])
-		joint_num.append(1)
-	
+		joint_s.append([joint[i][0], joint[i][1], joint[i][2], 1])
+
+#calculate reducers
+for i in range(1, len(joint_s)): #From 1 because the [0,0,0] joint
+	for j in range(0, 2):
+		knowndia_reducer_len[knowndia[joint_s[i][j]]] += 0.2*joint_s[i][3]
+	knowndia_reducer_len[knowndia[joint_s[i][2]]] += 0.25*joint_s[i][3]
+
+
+reducer.append([0,1,1])
+for i in range (1, len(joint_s)):
+	diff = 200 #lagre number
+	for j in range(2, 10): #len(knowndia_inv) #8 + 8 = 16, so D8 (D6) and D10 are not interesting, but we need d12 to diff not to be 200
+		if joint_s[i][1] == 6:#Use D8 instead of D6
+			r1 = 8
+		else:
+			r1 = joint_s[i][1]
+		if joint_s[i][2] == 6:
+			r2 = 8
+		else:
+			r2 = joint_s[i][2]
+		if abs(knowndia_inv[j] - (r1 + r2)) > diff or knowndia_inv[j] == 56: #What is the solution if  for example D28 + D35 has to reduce?
+			passed = False
+			if knowndia_inv[j] == 56 and abs(knowndia_inv[j] - (r1 + r2)) <= diff:
+				for k in range(0, len(reducer)):
+					if (reducer[k][0] == joint_s[i][0] or (reducer[k][0] == 8 and joint_s[i][0] == 6)) and reducer[k][1] == knowndia_inv[j]:
+						reducer[k][2] += joint_s[i][3]
+						passed = True
+						break
+			else:
+				for k in range(0, len(reducer)):
+					if (reducer[k][0] == joint_s[i][0] or (reducer[k][0] == 8 and joint_s[i][0] == 6)) and reducer[k][1] == knowndia_inv[j - 1]:
+						reducer[k][2] += joint_s[i][3]
+						passed = True
+						break
+			if passed == False:
+				if knowndia_inv[j] == 56 and abs(knowndia_inv[j] - (r1 + r2)) <= diff:
+					reducer.append([joint_s[i][0], knowndia_inv[j], joint_s[i][3]])
+				else:
+					reducer.append([joint_s[i][0], knowndia_inv[j - 1], joint_s[i][3]])
+				break
+			else:
+				break #Because I cannot break from this for loop in case of True
+		else:
+			diff = abs(knowndia_inv[j] - (r1 + r2))
+
+
+#Summarize the reducers
+reducer_s = list() #Summarized reducers #delete
+#reducer_pipe = [[8, 0], [10, 0], [12, 0], [15, 0], [18, 0], [22, 0], [28, 0], [35, 0], [42, 0], [56, 0]]#If the reducer is actually a straight pipe section [pipe diameter, pcs.]
+reducer_s.append([0,1,1])
+
+for i in range (1, len(reducer)):
+	d1 = max(reducer[i][0], reducer[i][1])
+	d2 = min(reducer[i][0], reducer[i][1])
+	if d1 != d2:
+		for j in range (0, len(reducer_s)):
+			passed = False
+			if reducer_s[j][0] == d1 and reducer_s[j][1] == d2:
+				reducer_s[j][2] = reducer[i][2]
+				passed = True
+				break
+		if passed == False:
+			reducer_s.append([d1, d2, reducer[i][2]])
+	else:
+		print('hy')
+		knowndia_reducer_len[knowndia[d1]] += 0.5
+
+reducer_s = sorted(reducer_s, key=itemgetter(0, 1))
+joint_s = sorted(joint_s, key=itemgetter(0, 1, 2))
+
 print()
 print('***End***')
 print()
 
+'''
+print('___Debug___')
 print(size)
 print("joints: " + str(joint_s))
-print('number of joints: ' + str(joint_num))
-print('pipe length 6, 10 12, 16, 20, 22, 28, 35, 42: ' + str(pipdia_len))
+print('pipe length 6, 10 12, 15, 18, 22, 28, 35, 42, 56: ' + str(pipdia_len))
+print('pipe length reducer 8, 10 12, 15, 18, 22, 28, 35, 42, 56: ' + str(knowndia_reducer_len))'''
 
 
+#Excell manipulation
 from openpyxl import Workbook
 from openpyxl import load_workbook
+from openpyxl.styles import Font, Color, colors
 infil = infil[0:len(infil)-4] + '_joint.xlsx' #Give a similar name to the output file
-try:
+try: #Not use existing file (to save our files, and it's contents)
 	wb_load = load_workbook(filename = infil)
 	print('\n***\n A futás sikertelen.\n')
-	print(infil + ' néven létezik már fájl a mappában, ennek felülírására a programnak nincs jogosultsága. Kérlek nevezd vagy helyezd át a ' + infil +' nevű fájlt (vagy töröld), majd utána futtasd újra a kódot!')
+	print(infil + ' néven létezik már fájl a mappában, ennek felülírására a programnak nincs jogosultsága. \nKérlek nevezd vagy helyezd át a ' + infil +' nevű fájlt (vagy töröld), majd utána futtasd újra a kódot!\nKilépéshez a control+c gombot nyomd meg!')
 	time.sleep(10)
 	sys.exit()
 except FileNotFoundError:
-	print('Hello')
 	pass
-	
+
+#Write an excell file
 wb = Workbook()
 ws = wb.active
-ws['A1'] = 1
+ft_info = Font(color = colors.RED, bold = True)
+ft_title = Font(bold = True)
+
+ws['A1'] = info #Write errors, and info-s
+a1 = ws['A1']
+a1.font = ft_info
+ws['A2'] = '1. csonk'
+ws['B2'] = '2. csonk'
+ws['C2'] = '3. csonk'
+ws['D2'] = 'Darab'
+ws.cell(column = 1, row = len(joint_s) + 5, value = 'D [mm]').font = ft_title
+ws.cell(column = 2, row = len(joint_s) + 5, value = 'l_csőhöz [m]').font = ft_title
+ws.cell(column = 3, row = len(joint_s) + 5, value = 'l_jointhoz [m]').font = ft_title
+ws.cell(column = 4, row = len(joint_s) + 5, value = 'l_össz [m]').font = ft_title
+ws.cell(column = 1, row = len(joint_s) + len(knowndia_inv) + 9, value = 'D1_szűkítő [mm]').font = ft_title
+ws.cell(column = 2, row = len(joint_s) + len(knowndia_inv) + 9, value = 'D1_szűkítő [mm]').font = ft_title
+ws.cell(column = 3, row = len(joint_s) + len(knowndia_inv) + 9, value = 'darab').font = ft_title
+a2 = ws['A2']
+b2 = ws['B2']
+c2 = ws['C2']
+d2 = ws['D2']
+a2.font = ft_title
+b2.font = ft_title
+c2.font = ft_title
+d2.font = ft_title
+ws['A3'] = 0
+ws['B3'] = 0
+ws['C3'] = 0
+ws['D3'] = 1
+for i in range (0, len(joint_s)):
+	for j in range (1, 5):
+		ws.cell(column = j, row = (i + 4), value = joint_s[i][j-1])
+
+ws.cell(column = 1, row = len(joint_s) + 6, value = knowndia_inv[0])
+ws.cell(column = 2, row = len(joint_s) + 6, value = pipdia_len[0])
+ws.cell(column = 3, row = len(joint_s) + 6, value = 0)
+ws.cell(column = 4, row = len(joint_s) + 6, value = pipdia_len[0])
+
+ws.cell(column = 1, row = len(joint_s) + 7, value = 8 ) #Because of the reducers
+ws.cell(column = 2, row = len(joint_s) + 7, value = 0)
+ws.cell(column = 3, row = len(joint_s) + 7, value = knowndia_reducer_len[0])
+ws.cell(column = 4, row = len(joint_s) + 7, value = knowndia_reducer_len[0])
+
+for i in range(1, len(knowndia_inv)):#Error, the known diameters, are not defined properly yet.
+	ws.cell(column = 1, row = len(joint_s) + 7 + i, value = knowndia_inv[i])
+	ws.cell(column = 2, row = len(joint_s) + 7 + i, value = pipdia_len[i])
+	ws.cell(column = 3, row = len(joint_s) + 7 + i, value = knowndia_reducer_len[i])
+	ws.cell(column = 4, row = len(joint_s) + 7 + i, value = pipdia_len[i] + knowndia_reducer_len[i])
+
+for i in range(0, len(reducer)):#Error, the known diameters, are not defined properly yet.
+	ws.cell(column = 1, row = len(joint_s) + len(knowndia_inv) + 10 + i, value = reducer_s[i][0])
+	ws.cell(column = 2, row = len(joint_s) + len(knowndia_inv) + 10 + i, value = reducer_s[i][1])
+	ws.cell(column = 3, row = len(joint_s) + len(knowndia_inv) + 10 + i, value = reducer_s[i][2])
+	
 wb.save(infil)
 
+print('A program futása sikeres volt. \nKilépéshez nyomd meg a control+c gombot, vagy várj 5 másodpercet!')
+time.sleep(5)
+sys.exit()
+
+
 '''
-from openpyxl import Workbook
-wb = Workbook()
-ws = wb.active
-print('A' + str(i))
-ws.cell(row=4, column=2, value=10)
-ws['A2'] = 2
-print(ws['B2'])
-wb.save('NDT_eport.xlsx')
-
-from openpyxl import load_workbook
-wb = load_workbook(filename = 'empty_book.xlsx')
-sheet_ranges = wb['Sheet']
-print(sheet_ranges['D18'].value)'''
-
-
 print('****Test section starts here******')
 print('****End******')
-#Find/ask the input xml file
+'''
 #bent -> pipe length in the export?
 #outdoor unit joint calculate #Bug: In case of two (or more?) OU one of them pipe doesent calculate properly (just the liquid pipe summarize in the excell export, but the gas pipe is not summarized)
-#calculate the special cases
-#create export file
-#Summarize more export files
-#Check from the NDT .xlsx export file
 #-1 return value -> error message; + do not cause error
-#piplenA, piplenB.... create and calculate
-#reducer might have to use in case of PAC-LV (because of the PAC has D10 and D12 diameters)
+#Reducer diameters: 6, 8, 10, 12, 16, 18 (soft);22, 28, 35, 42, 56
+#Reducer pipe: 2x20cm + from the smallest is 25cm
+#If reducer D1 == reducer D2, than use ??m pipe
